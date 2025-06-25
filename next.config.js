58d3f0f2f399abecdbde6ea53110725b7c3eb1e8
Maria Evellyn da silva <evellynsilvadeveloper@gmail.com>
2025-06-25 07:20:28 -0300

    Feat: Consertando o erro na build

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost'], // Adicione seus domínios de imagem
+  },
+}
+
+module.exports = nextConfig
