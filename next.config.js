995fc082a638d0594027e25e1e825683acdc9d88
Maria Evellyn da silva <evellynsilvadeveloper@gmail.com>
2025-06-25 07:22:35 -0300

    Feature: Testando conserto da build

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['localhost'], // Adicione seus domínios de imagem
-  },
-}
-
-module.exports = nextConfig
